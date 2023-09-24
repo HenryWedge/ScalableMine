91d598ef422ee55a6e584e85c64d8b4671005b1f
Hendrik Reiter <hendrik.reiter@yahoo.com>
2023-09-24 07:30:22 +0200

    adjust build for the cluster

---
+++ scripts/push_docker.sh
@@ -0,0 +1,3 @@
+docker push harbor.se.internal/hre/process-model-creator:latest
+docker push harbor.se.internal/hre/process-mining-data-processor:latest
+docker push harbor.se.internal/hre/sample-data-generator:latest
